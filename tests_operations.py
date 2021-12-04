97f07adf0310f3a4bbeecf1cd764ea3b46b9f342
James Lee <jameslee6352@gmail.com>
2021-12-04 11:22:05 +0800

    added tests

--- tests_operations.py
+++ tests_operations.py
@@ -34,6 +34,10 @@ def test_division():
     WHEN:  passed in to the function division
     THEN:  the resulting is the division of the two number
     """
+    assert division(8,4) == 4
+    assert division(0,5) == 5
+    assert division(1000000,1) == 1000000
+
     pass
 
 
