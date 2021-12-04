564b955198e5489a1642dbf2e8b1eb5d4526fa28
Bryce O'Connor <contentaggregator@mail.com>
2021-12-04 11:23:30 +0800

    Added tests to function.

--- tests_operations.py
+++ tests_operations.py
@@ -25,8 +25,9 @@ def test_multiplication():
     WHEN:  passed in to the function multiplication
     THEN:  the resulting is the multiplication of the two numbers
     """
-    pass
-
+    assert multiplication(3, 5) == (15)
+    assert multiplication(0, 4) == (0)
+    assert multiplication(1, -7) == (-7)
 
 def test_division():
     """
