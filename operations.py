b92171fc1d49bd5cc7f312f09d71b64982c1fa26
Jake Lyell <50351260+JakeL1001@users.noreply.github.com>
2021-12-04 11:21:15 +0800

    s4 Added addition function to operations.py (#20)
    * Added addition function to operations.py
    
    * Documented Function

--- operations.py
+++ operations.py
@@ -1,13 +1,6 @@
 def addition(num1, num2):
-    """
-    This function will perform a addition operation on num1 by num2
-
-    Note: This function is very simplified... but for the purpose of this exercise
-    Imagine that you are building a very complex function, and you want to be able to
-    work with multiple people.
-    """
-    # Add code here
-    pass
+    # Returns the sum of num1 and num2
+    return (num1 + num2)
 
 
 def subtraction(num1, num2):
